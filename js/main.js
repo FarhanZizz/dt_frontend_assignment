3dbfa1520142fd23a448f9196bc03a5780d9ebbe
Farhan Aziz <iamfarhanaziz@gmail.com>
2026-01-09 16:39:41 +0600

    [Done] Task Header

--- js/main.js
+++ js/main.js
@@ -16,9 +16,18 @@ function initializePage(projectData) {
   console.log("First Task:", projectData);
 
   renderProjectHeader(projectData.title);
+  renderTaskHeader(projectData.tasks[0]);
 }
 
 function renderProjectHeader(title) {
   const projectTitleElem = document.getElementById("projectTitle");
   projectTitleElem.textContent = title || "Project Title";
+}
+
+function renderTaskHeader(task) {
+  const taskTitle = document.getElementById("taskTitle");
+  const taskDescription = document.getElementById("taskDescription");
+
+  taskTitle.textContent = task.task_title || "Task Title";
+  taskDescription.textContent = task.task_description || "Task Description";
 }
